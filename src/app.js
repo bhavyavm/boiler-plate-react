a36cf0b46c16131b545395061a3c14bba53fe170
bhavya <bhavyavmohan@gmail.com>
2020-05-05 10:57:21 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/style.css'
+
+const root = document.getElementById('app');
+ReactDOM.render(<p>boiler plate</p>, root)
